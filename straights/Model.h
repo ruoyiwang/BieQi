075aad16a8b24219a1dc4cbc4e7ebf8802281ddd
huanbing <wang-ruoyi@163.com>
2012-07-11 23:56:14 -0400

    first added the models, not complete at all

---
+++ straights/Model.h
@@ -0,0 +1,19 @@
+#include <vector>
+#include <cstdlib>
+#include "Player.h"
+#include "Referee.h"
+
+class Model{
+private:
+	int iSeed;
+	int iCurrentPlayer;
+	std::vector<Player*> playerList;
+	std::vector<Player*> gamePlayerList;
+	Table cardTable;
+	Referee referee;
+public:
+	Model();
+	~Model();
+	void gameStart(bool bHuman1, bool bHuman2, bool bHuman3, bool bHuman4);
+
+};
